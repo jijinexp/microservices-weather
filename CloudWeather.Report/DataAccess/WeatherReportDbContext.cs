65c694fd9568e2699553e418bc420501fca7ef32
Johney <jijinexp@gmail.com>
2022-12-30 09:58:23 +1300

    Adding Data Access service to Temperature and Weather Report

---
+++ CloudWeather.Report/DataAccess/WeatherReportDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+public class WeatherReportDbContext: DbContext{
+    public WeatherReportDbContext()
+    {
+        
+    }
+    public WeatherReportDbContext(DbContextOptions opts):base(opts){}
+    public DbSet<WeatherReport> WeatherReports {get; set;}
+    protected override void OnModelCreating(ModelBuilder modelBuilder){
+        base.OnModelCreating(modelBuilder);
+        SnakeCaseIdentityTableNames(modelBuilder);
+    }
+    private static void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder){
+        modelBuilder.Entity<WeatherReport>(b => {b.ToTable("weather_report");});
+    }
+
+}
