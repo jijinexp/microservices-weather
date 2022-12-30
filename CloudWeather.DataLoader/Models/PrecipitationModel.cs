3e561340883c3ab3d45045f4e633a6df4391ef1c
Johney <jijinexp@gmail.com>
2022-12-30 18:46:52 +1300

    Adding DataLoader

---
+++ CloudWeather.DataLoader/Models/PrecipitationModel.cs
@@ -0,0 +1,6 @@
+internal class PrecipitationModel{
+    public DateTime CreatedOn { get; set; }
+    public decimal AmountInches { get; set; }
+    public string WeatherType { get; set; }
+    public string ZipCode { get; set; }
+}
